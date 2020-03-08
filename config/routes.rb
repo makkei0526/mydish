8449535591d292ede4b46289ef78731177ea34aa
makkei0526 <makkei1710@gmail.com>
2020-03-08 22:56:51 +0900

    リンクをヘルパーメソッドに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'dishes#index'
-  get 'dishes', to: 'dishes#index'
-  get 'dishes/new', to: 'dishes#new'
-  post 'dishes', to: 'dishes#create'
-  get 'dishes/:id', to: 'dishes#show'
+  resources :dishes, only: [:index, :new, :create, :show]
 end
