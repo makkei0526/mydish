43133af1b36747a2c6a33c74bc7e46290b7e3561
makkei0526 <makkei1710@gmail.com>
2020-03-08 21:58:46 +0900

    料理新規投稿ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'dishes#index'
+  # root 'dishes#index'
+  get 'dishes', to: 'dishes#index'
+  get 'dishes/new', to: 'dishes#new'
 end
