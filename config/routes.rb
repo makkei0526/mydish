127ce04dccd024de5d7b8af8b29ed545d394d810
makkei0526 <makkei1710@gmail.com>
2020-03-09 13:26:50 +0900

    ヘッダーメニューの追加、ヘルパーメソッドの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'dishes#index'
+  root 'home#index'
   resources :dishes
   resources :comments, only: %i[create destroy]
 end
