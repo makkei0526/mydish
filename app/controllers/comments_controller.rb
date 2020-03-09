44d06bac7a2a5d8bc1058503230181fee0896891
makkei0526 <makkei1710@gmail.com>
2020-03-09 12:00:40 +0900

    コメント機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,20 @@
 class CommentsController < ApplicationController
   def create
-
     comment = Comment.new(comment_params)
     if comment.save
       flash[:notice] = 'コメントを投稿しました'
       redirect_to comment.dish
     else
-      # redirect_to :back, flash: {
-      #   comment: comment,
-      #   error_messages: comment.errors.full_messages
-      # }
       flash[:comment] = comment
-      flash[:error_messages] = comment.errors.full.messages
-      redirect_back fall_location: comment.dish
+      flash[:error_messages] = comment.errors.full_messages
+      redirect_back fallback_location: comment.dish
     end
   end
 
   def destroy
+    comment = Comment.find(params[:id])
+    comment.delete
+    redirect_to comment.dish, flash: { notice: 'コメントが削除されました'}
   end
 
   private
