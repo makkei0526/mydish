2d4cf90a490580cbb9cc2c0e7fc9c603e76db02d
makkei0526 <makkei1710@gmail.com>
2020-03-15 15:48:23 +0900

    トップページを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'home', to: 'home#index'
   get 'mypage', to: 'users#me'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'dishes#index'
   resources :users, only: %i[new create]
   resources :dishes
   resources :comments, only: %i[create destroy]
